83d4394794766b90e381848bd9a179a028304c48
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-06-29 10:00:31 +0530

    Functions

---
+++ StriversA2ZDSACourse/Step1/Step1.1/functions.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> passedBy(int a, int &b) {
+        vector<int> pass;
+        pass.push_back(a+1);
+        pass.push_back(b+2);
+        return pass;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        Solution obj;
+        vector<int> ans = obj.passedBy(a, b);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
