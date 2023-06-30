f2b1754eba5304c582e7de85c3d9f572266e7f9e
Shubham Aggarwal <89722744+Shubham-Aggarwal-1306@users.noreply.github.com>
2023-06-30 18:11:37 +0530

    Armstrong

---
+++ StriversA2ZDSACourse/Step1/Step1.4/Armstrong.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string armstrongNumber(int n){
+        // code here
+        int num = n;
+        int sum = 0;
+        while (n>0){
+            int curr = n%10;
+            sum += curr*curr*curr;
+            n = n/10;
+        }
+        if (sum == num){
+            return "Yes";
+        } else {
+            return "No";
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
