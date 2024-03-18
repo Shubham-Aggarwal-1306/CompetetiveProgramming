bb9d9befd80847e688bea40060bd0a4be0465624
Shubham Aggarwal <aggarwalshubham026@gmail.com>
2024-03-18 20:21:33 +0530

    Ceil The Floor

---
+++ StriversA2ZDSACourse/Step4/Step4.1/CeilTheFloor.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+pair<int, int> getFloorAndCeil(vector<int> &a, int n, int x) {
+  // Write your code here.
+
+  int low = 0, high = n - 1, floor = -1, ciel = -1;
+
+  while (low <= high) {
+    int mid = (low + high) / 2;
+    if (a[mid] > x) {
+      ciel = a[mid];
+      high = mid - 1;
+    } else if (a[mid] < x) {
+      floor = a[mid];
+      low = mid + 1;
+    } else {
+      floor = a[mid];
+	  ciel = a[mid];
+	  break;
+    }
+  }
+  return {floor, ciel};
+}
