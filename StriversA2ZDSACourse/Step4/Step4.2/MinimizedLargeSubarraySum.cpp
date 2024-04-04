5ef4a7b7863152d6c3fa0b78fce7608cbd5301bf
Shubham Aggarwal <aggarwalshubham026@gmail.com>
2024-04-04 23:27:32 +0530

    Minimized Largest Sub Array Sum

---
+++ StriversA2ZDSACourse/Step4/Step4.2/MinimizedLargeSubarraySum.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int noOfArrays(int num, vector<int> a) {
+  int sum = 0;
+  int n = 1;
+  for (auto i : a) {
+    if (sum + i > num) {
+      sum = i;
+      n++;
+    } else {
+      sum += i;
+    }
+  }
+  return n;
+}
+
+int largestSubarraySumMinimized(vector<int> a, int k) {
+  // Write Your Code Here
+  int low = *max_element(a.begin(), a.end());
+  int high = accumulate(a.begin(), a.end(), 0);
+
+  while (low <= high) {
+    int mid = (low + high) / 2;
+    int s = noOfArrays(mid, a);
+    if (s > k) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return low;
+}
